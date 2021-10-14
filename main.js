63dcadf17196d11e455be0641c16f82745273b46
Peter11231 <92013839+Peter11231@users.noreply.github.com>
2021-10-14 18:46:59 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function reverseString(message) {
- 
-    var splitstr = message.split(""); 
-    var rearr = splitstr.reverse(); 
-    var joint = rearr.join(""); 
-    return joint; 
-}
- 
-console.log( reverseString("hello"))
